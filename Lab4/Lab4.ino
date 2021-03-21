7e449309a8b71e20409a2b7d9dc336f156ca3fe0
agu18044 <78278462+agu18044@users.noreply.github.com>
2021-03-21 17:07:30 -0600

    cambios varios

--- Lab4/Lab4.ino
+++ Lab4/Lab4.ino
@@ -16,11 +16,13 @@
 #define LEDG PF_3
 #define LEDB PF_2
 int cont1 = 0;
-int led1[] = {23,24,25,26};
-int band = 0;
+int cont2 = 0;
+int led1[] = {23,24,25,26,27,28,29,10};
+int led2[] = {19,18,15,14,13,12,11,32};
+int band = 1;
 const int player1 = PUSH1;
 const int player2 = PUSH2;
-const int start = PE_2;
+const int start = PB_5;
 int state0 = 0;
 int state1 = 0;
 int state2 = 0;
@@ -30,6 +32,8 @@ int state2 = 0;
 //#define LED GREEN_LED
 
 void semaforo (void);  
+void juego (void);
+
 
 // the setup routine runs once when you press reset:
 void setup() {                
@@ -40,8 +44,11 @@ void setup() {
   pinMode (start, INPUT_PULLUP); 
   pinMode (player1, INPUT_PULLUP);
   pinMode (player2, INPUT_PULLUP);      
-  for (int i=0; i<4; i++){
+  for (int i=0; i<8; i++){
     pinMode(led1[i], OUTPUT);
+    }
+  for (int i=0; i<8; i++){
+    pinMode(led2[i], OUTPUT);
     }
   }
 
@@ -51,15 +58,10 @@ void loop() {
   state2 = digitalRead(player2);
   if (state0 == LOW){
     semaforo();
+    band = 0;
     }
-  if (state1 == LOW){
-    if (cont1 >= 4){
-      cont1 = 0;
-      }
-    cont1 ++;
-    digitalWrite(led1[cont1], HIGH);
-    delay(250);
-    digitalWrite(led1[cont1], LOW);
+  if (band == 0){
+    juego();
     }
 }
 /// secuencia de semaforo
@@ -78,4 +80,25 @@ void semaforo (void){
   digitalWrite(LEDG, LOW);
   digitalWrite(LEDB, HIGH);
   delay(1000);
+  }
+
+void juego (void){
+  if (state1 == LOW){
+    if (cont1 >= 8){
+      cont1 = 0;
+      }
+    cont1 ++;
+    digitalWrite(led1[cont1], HIGH);
+    delay(250);
+    digitalWrite(led1[cont1], LOW);
+    }
+  if (state2 == LOW){
+    if (cont2 >= 8){
+      cont2 = 0;
+      }
+    cont2 ++;
+    digitalWrite(led2[cont2], HIGH);
+    delay(250);
+    digitalWrite(led2[cont2], LOW);
+    }
   }
