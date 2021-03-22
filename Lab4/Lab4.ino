1379dc22fa9eb006e9785bc029c54f80304fab8b
agu18044 <78278462+agu18044@users.noreply.github.com>
2021-03-22 13:50:23 -0600

    cambios al contador y led de ganador

--- Lab4/Lab4.ino
+++ Lab4/Lab4.ino
@@ -17,39 +17,50 @@
 #define LEDB PF_2
 int cont1 = 0;
 int cont2 = 0;
-int led1[] = {23,24,25,26,27,28,29,10};
-int led2[] = {19,18,15,14,13,12,11,32};
+
+int led1[] = {PB_0, PB_1, PB_2, PB_3, PB_4, PB_5, PB_6, PB_7};
+int led2[] = {PD_0, PD_1, PD_2, PD_3, PD_6, PD_7, PE_1, PE_2};
+
 int band = 1;
 const int player1 = PUSH1;
 const int player2 = PUSH2;
-const int start = PB_5;
+const int start = PA_2;
 int state0 = 0;
 int state1 = 0;
 int state2 = 0;
 
 
-//see pins_energia.h for more LED definitions
-//#define LED GREEN_LED
-
+// declaración de funciones
 void semaforo (void);  
 void juego (void);
 
 
-// the setup routine runs once when you press reset:
-void setup() {                
-  // initialize the digital pin as an output.
+
+void setup() {  
+  cont1 = 0;
+  cont2 = 0;
+  // Semaforo              
   pinMode(LEDR, OUTPUT);
   pinMode(LEDB, OUTPUT);
   pinMode(LEDG, OUTPUT); 
+  digitalWrite(LEDR, LOW);
+  digitalWrite(LEDB, LOW);
+  digitalWrite(LEDG, LOW);
+  // botones para iniciar y jugar
   pinMode (start, INPUT_PULLUP); 
   pinMode (player1, INPUT_PULLUP);
-  pinMode (player2, INPUT_PULLUP);      
+  pinMode (player2, INPUT_PULLUP); 
+  // contador de leds     
   for (int i=0; i<8; i++){
     pinMode(led1[i], OUTPUT);
+    digitalWrite(led1[i], LOW);
     }
   for (int i=0; i<8; i++){
     pinMode(led2[i], OUTPUT);
+    digitalWrite(led2[i], LOW);
     }
+  digitalWrite(led1[0], HIGH);
+  digitalWrite(led2[0], HIGH);
   }
 
 void loop() {
@@ -64,8 +75,9 @@ void loop() {
     juego();
     }
 }
-/// secuencia de semaforo
-
+/////////////////////////////////////////////////////////////
+//                   secuencia de semaforo                 //
+/////////////////////////////////////////////////////////////
 void semaforo (void){
   digitalWrite(LEDB, LOW);  
   digitalWrite(LEDR, HIGH);    
@@ -82,23 +94,38 @@ void semaforo (void){
   delay(1000);
   }
 
+/////////////////////////////////////////////////////////////
+//                   secuencia de juego                    //
+/////////////////////////////////////////////////////////////
 void juego (void){
   if (state1 == LOW){
-    if (cont1 >= 8){
-      cont1 = 0;
-      }
+    digitalWrite(led1[0], LOW);
     cont1 ++;
     digitalWrite(led1[cont1], HIGH);
-    delay(250);
+    delay(500);
     digitalWrite(led1[cont1], LOW);
     }
   if (state2 == LOW){
-    if (cont2 >= 8){
-      cont2 = 0;
-      }
+    digitalWrite(led2[0], LOW);
     cont2 ++;
     digitalWrite(led2[cont2], HIGH);
-    delay(250);
+    delay(500);
     digitalWrite(led2[cont2], LOW);
     }
-  }
+  if (cont1 >= 8){
+    cont1 = 0;
+    band = 1;
+    digitalWrite(LEDB, LOW);
+    digitalWrite(LEDR, HIGH); 
+    delay(500);
+    setup();
+    }
+  if (cont2 >= 8){
+    cont2 = 0;
+    band = 1;
+    digitalWrite(LEDB, LOW);
+    digitalWrite(LEDG, HIGH); 
+    delay(500);
+    setup();
+    } 
+ }
